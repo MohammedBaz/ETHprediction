c540b2afc6ef08ed7da57a9be16a44c15135aa22
MohammedBaz <55236963+MohammedBaz@users.noreply.github.com>
2022-07-12 16:52:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,4 @@
-
+! pip3 install ethereum-etl
+ ethereumetl export_blocks_and_transactions --start-block 0 --end-block 500000 \
+--blocks-output blocks.csv --transactions-output transactions.csv \
+--provider-uri "https://mainnet.infura.io/v3/e95417e91b974e3d81dfc2bfe40aa2c6"
